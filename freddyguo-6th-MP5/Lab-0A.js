8fa53699cd5290902c73fe5b25381208f19dddaa
Freddy Guo <freddyguo@MacBook-Pro.local>
2020-09-22 14:24:18 -0500

    lab 2

--- freddyguo-6th-MP5/Lab-0A.js
+++ freddyguo-6th-MP5/Lab-0A.js
@@ -1,5 +1,5 @@
-console.log('Freddy Guo')
-console.log('My favorite color is blue.')
-console.log('My favorite song is Glorious.')
-console.log('My favorite tv program is the NBA.')
-console.log('My favorite food is pizza.')
+console.log('Freddy Guo');
+console.log('My favorite color is blue.');
+console.log('My favorite song is Glorious.');
+console.log('My favorite tv program is the NBA.');
+console.log('My favorite food is pizza.');
---
+++ freddyguo-6th-MP5/Lab-0B.js
@@ -0,0 +1,10 @@
+console.log("2 + 8 - 5");
+console.log(2 + 8 - 5);
+
+                 
+console.log("2 * 8 - 5");
+console.log(2 * 8 - 5);
+
+                 
+console.log("2 * 8 / 4");
+console.log(2 * 8 / 4);

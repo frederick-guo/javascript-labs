832580f709bfd3a1a32ffdd2913f2c29ddb8ec24
Freddy Guo <freddyguo@MacBook-Pro.local>
2020-10-28 14:33:08 -0500

    finished lab 35

--- Q2/freddyguo-6th-MP2/Lab-35.js
+++ Q2/freddyguo-6th-MP2/Lab-35.js
@@ -14,8 +14,20 @@ class Student{
 
 function printStudents (students){
     for(let i = 0; i < students.length; i++){
-        console.log(`${student.getFirstNameLastName()} ${student.q1} ${student.q2} ${student.average}`)
+        console.log(`${students[i].getFirstNameLastName()} ${students[i].q1} ${students[i].q2} ${students[i].average}`)
     }
+}
+
+function findTheStudentWithTheHighesAverage(students){
+    let studentWithHighestAverage = students[0]
+
+    for(let i = 1; i < students.length; i++){
+        if (students[i].average > studentWithHighestAverage.average){
+            studentWithHighestAverage = students[i]
+        }
+    }
+
+    return studentWithHighestAverage;
 }
 
 const readline = require('readline-sync');
@@ -23,7 +35,7 @@ const readline = require('readline-sync');
 students = [];
 
 while(true){
-    firstName = readline.question("Enter the person's name: ")
+    firstName = readline.question("\nEnter the person's first name: ")
     if(firstName === ''){
         break;
     }
@@ -37,5 +49,12 @@ while(true){
     students.push(student);
 }
 
+console.log('\nList of Students: \n');
+
 printStudents(students);
 
+//I'm not sure if there's a function for javascript that can replicate "attrgetter" for python
+
+console.log('\nStudent with the Highest Average: ')
+
+console.log(findTheStudentWithTheHighesAverage(students).firstName + ' ' + findTheStudentWithTheHighesAverage(students).lastName);

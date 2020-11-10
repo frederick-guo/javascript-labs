7827cea40850595a54c52eb6520d2d65dfe9e5d0
Freddy Guo <freddyguo@MacBook-Pro.local>
2020-11-10 14:32:08 -0600

    finished lab 40 and added math npm

---
+++ Q2/freddyguo-6th-MP4/Lab-40.js
@@ -0,0 +1,18 @@
+function printMatrix(matrix){
+    for(let r = 0; r < matrix.length; r++){
+        for(let c = 0; c < matrix[r].length; c++){
+            process.stdout.write(matrix[r][c] + ' ')
+        }
+        console.log()
+    }
+}
+
+
+const one = ([ [1, 3, 6, 8,14,16,18], [1, 2, 3, 4, 5, 6, 7], [1, 2, 8, 4, 9, 6, 6] ])
+				   				  
+const two = ([ [2, 3, 6, 8,14,16,18], [3, 2, 3, 4, 5, 6, 7], [4, 2, 8, 4, 9, 6, 6] ])
+
+
+printMatrix(one)
+console.log()
+printMatrix(two)
